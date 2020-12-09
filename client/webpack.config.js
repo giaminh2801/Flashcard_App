dff6398f52da00834927c48a5ca0dbb29fd4ddb9
Minh Le <silbullet2801@gmail.com>
2020-12-09 17:13:37 +0700

    Webpack react first-step

---
+++ client/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, "src", "index.js")
+    },
+    output: {
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html")
+        })
+    ]
+};
